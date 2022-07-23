c2dd29e63a459c955ea72c14db7ac21a43b4886c
Shub Raj Lama <shuvraj1234@gmail.com>
2022-07-23 09:02:42 +0545

    exec used instead of spawn

--- core/webtor.js
+++ core/webtor.js
@@ -1,15 +1,18 @@
 import WebTorrent from 'webtorrent-hybrid';
 import path from "node:path";
-import { spawn } from 'node:child_process';
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
 import addQueue from './utils.js';
+
+const execP = promisify(exec);
 class TorClient {
     constructor() {
         this.client = new WebTorrent();
         this.odir = path.resolve("./downloads");
     }
-    uploadToDropbox(sourceDIR) {
-        const pprocess = spawn("python3", ["core/dpbox.py", `${sourceDIR}`,"&"]);
-    }
+    // uploadToDropbox(sourceDIR) {
+    //     spawn("python3", ["core/dpbox.py", `${sourceDIR}`, "&"]);
+    // }
     addMagnet(magnetURI) {
         this.client.add(magnetURI, { path: this.odir, destroyStoreOnDestroy: false, storeCacheSlots: 0 }, async (torrent) => {
             console.log('Client is downloading:', torrent.infoHash);
@@ -23,7 +26,7 @@ class TorClient {
             })
             torrent.on("done", async () => {
                 torrent.destroy();
-                this.uploadToDropbox(path.join(this.odir, torrent.name));
+                const { stdout, stderr } = await execP(`python3 core/dpbox.py "${path.join(this.odir, torrent.name)}"`);
                 await addQueue();
             });
         });
