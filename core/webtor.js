4a4cc05e11d5b792c4f813cfed390f5a05654a5f
Shub Raj Lama <shuvraj1234@gmail.com>
2022-07-25 13:10:48 +0545

    log added

--- core/webtor.js
+++ core/webtor.js
@@ -15,16 +15,26 @@ class TorClient {
     }
     addMagnet(magnetURI) {
         this.client.add(magnetURI, { path: this.odir, destroyStoreOnDestroy: false, storeCacheSlots: 0 }, async (torrent) => {
+            console.log(`Started downloading ${torrent.name}`);
+            torrent.on("download", (bytes) => {
+                let downloadSpeed = (torrent.downloadSpeed/(1024*1024)).toFixed(2);
+                if (downloadSpeed < 0.2){
+                    torrent.emit("noPeers");
+                }
+                console.log(`Downloading ${torrent.name}\tProgress: ${torrent.progress.toFixed(2)}\tSpeed: ${downloadSpeed}`);
+            })
             console.log('Client is downloading:', torrent.infoHash);
             torrent.on("error", async () => {
                 torrent.destroy();
                 await addQueue();
             })
             torrent.on('noPeers', async () => {
+                console.log(`Downloaded Aborted no/weak Peers Found`);
                 torrent.destroy();
                 await addQueue();
             })
             torrent.on("done", async () => {
+                console.log(`Done downloading ${torrent.name}`);
                 torrent.destroy();
                 this.uploadToDropbox(path.join(this.odir, torrent.name));
             });
